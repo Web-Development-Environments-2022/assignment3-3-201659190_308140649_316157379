662976c75b7784828c8cce01fce1fc69971c5b50
Shahar <Shaharhadad123@gmail.com>
2022-08-20 19:57:57 +0300

    MyRecipePage in process

--- src/pages/MyRecipesPage.vue
+++ src/pages/MyRecipesPage.vue
@@ -1,13 +1,45 @@
 <template>
   <div class="container">
-  <h1 class="title">My Recipes Page</h1>
+    <h1 class="title">My Recipes Page</h1>
+    <div v-for="(r, index) in recipes" :key="index">
+      <RecipePreviewVue :recipe="r"></RecipePreviewVue>
+    </div>
 
+    
   
   </div>
 </template>
 
 <script>
+import RecipePreviewVue from '../components/RecipePreview.vue';
+export default {
+  components:{
+    RecipePreviewVue: RecipePreviewVue,
+  },
+  mounted() {
+    this.updateRecipes();
+  },
+  methods: {
+    async updateRecipes() {
+      try {
+        const response = await this.axios.get(
+          this.$store.server_domain + "/users/my_recipes",
+          {
+            withCredentials: true,
+          }
+        );
 
+        // console.log(response);
+        const recipes = response.data;
+        this.recipes = [];
+        this.recipes.push(...recipes);
+        // console.log(this.recipes);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+  }
 
 </script>
 
